d4468ac1edcd44e87a27ae1afb22b1978cada719
v-raras <50567545+v-raras@users.noreply.github.com>
2019-06-07 15:15:28 +0530

    Update MultilpleSolutionsImport.ps1

--- MultilpleSolutionsImport.ps1
+++ MultilpleSolutionsImport.ps1
@@ -16,15 +16,15 @@ $logsDirectory,
 $logFilename
 )
 
-Write-Host $solutionListFile,
-Write-Host $solutionImportPath,
-Write-Host $crmConnectionString,
-Write-Host $override,
-Write-Host $publishWorkflows,
-Write-Host $overwriteUnmanagedCustomizations,
-Write-Host $skipProductUpdateDependencies,
-Write-Host $convertToManaged,
-Write-Host $holdingSolution,
-Write-Host $AsyncWaitTimeout,
-Write-Host $logsDirectory,
+Write-Host $solutionListFile
+Write-Host $solutionImportPath
+Write-Host $crmConnectionString
+Write-Host $override
+Write-Host $publishWorkflows
+Write-Host $overwriteUnmanagedCustomizations
+Write-Host $skipProductUpdateDependencies
+Write-Host $convertToManaged
+Write-Host $holdingSolution
+Write-Host $AsyncWaitTimeout
+Write-Host $logsDirectory
 Write-Host $logFilename
