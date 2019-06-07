1d295e4921780d25482c88bfe3152109971be654
v-raras <50567545+v-raras@users.noreply.github.com>
2019-06-07 15:20:48 +0530

    Update MultilpleSolutionsImport.ps1

--- MultilpleSolutionsImport.ps1
+++ MultilpleSolutionsImport.ps1
@@ -16,15 +16,27 @@ $logsDirectory,
 $logFilename
 )
 
-Write-Host $solutionListFile
-Write-Host $solutionImportPath
-Write-Host $crmConnectionString
-Write-Host $override
-Write-Host $publishWorkflows
-Write-Host $overwriteUnmanagedCustomizations
-Write-Host $skipProductUpdateDependencies
-Write-Host $convertToManaged
-Write-Host $holdingSolution
-Write-Host $AsyncWaitTimeout
-Write-Host $logsDirectory
-Write-Host $logFilename
+$ErrorActionPreference = "Stop"
+
+Write-Verbose 'Entering ImportSolution.ps1'
+ 
+if(-Not (Get-Module -ListAvailable -Name Xrm.Framework.CI.PowerShell.Cmdlets))
+{
+Import-Module $env:DOWNLOADSECUREFILE1_SECUREFILEPATH
+Import-Module $env:DOWNLOADSECUREFILE2_SECUREFILEPATH
+Import-Module $env:DOWNLOADSECUREFILE3_SECUREFILEPATH
+Import-Module $env:DOWNLOADSECUREFILE4_SECUREFILEPATH
+Import-Module $env:DOWNLOADSECUREFILE5_SECUREFILEPATH
+Import-Module $env:DOWNLOADSECUREFILE6_SECUREFILEPATH
+}
+
+
+foreach($solution in [System.IO.File]::ReadLines($solutionListFile)){
+    Write-Host "Getting " $solution.Trim() " from zip"
+    $solutionFile = $solutionImportPath + "\" + $solution.Trim()
+    $solutionInfo = Get-XrmSolutionInfoFromZip -SolutionFilePath "$solutionFile"
+
+    Write-Host "Solution Name: " $solutionInfo.UniqueName
+    Write-Host "Solution Version: " $solutionInfo.Version
+
+    }
