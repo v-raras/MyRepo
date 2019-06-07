45a0404c2d00cfa305578f762132142cf8ee009e
v-raras <50567545+v-raras@users.noreply.github.com>
2019-06-07 15:07:23 +0530

    Update MultilpleSolutionsImport.ps1

--- MultilpleSolutionsImport.ps1
+++ MultilpleSolutionsImport.ps1
@@ -1,19 +1,30 @@
-﻿#
+#
 # Filename: ImportSolution.ps1
 #
 param(
-$solutionListFile, #= "C:\Users\m-rrastogi\Downloads\Powershell ImportSolution files\solutions.txt", #The absolute path to the Solution.txt file zip to be imported
-$solutionImportPath, #= "C:\Users\m-rrastogi\Downloads\Powershell ImportSolution files", #The absolute path to the current directory
-
-#[string]$solutionFile, #The absolute path to the solution file zip to be imported
-$crmConnectionString,# = "AuthType=Office365;Username=v-raras@msdyn.onmicrosoft.com;Password=Raja7*kaka;Url=https://msdyn.crm8.dynamics.com",
-$override,#, = 1, #If set to 1 will override the solution even if a solution with same version exists
-$publishWorkflows,#, = 1, #Will publish workflows during import
-$overwriteUnmanagedCustomizations,#, = 1, #Will overwrite unmanaged customizations
-$skipProductUpdateDependencies,#, = 1, #Will skip product update dependencies
-$convertToManaged, #Direct the system to convert any matching unmanaged customizations into your managed solution. Optional.
+$solutionListFile,
+$solutionImportPath,
+$crmConnectionString,
+$override,
+$publishWorkflows,
+$overwriteUnmanagedCustomizations,
+$skipProductUpdateDependencies,
+$convertToManaged,
 $holdingSolution,
-$AsyncWaitTimeout, #Optional - Async wait timeout in seconds
-$logsDirectory,#, = "C:\Users\m-rrastogi\Downloads\Powershell ImportSolution files\SolutionImportLog", #Optional - will place the import log in here
-$logFilename #Optional - will use this as import log file name
+$AsyncWaitTimeout,
+$logsDirectory,
+$logFilename
 )
+
+Write-Host $solutionListFile,
+Write-Host $solutionImportPath,
+Write-Host $crmConnectionString,
+Write-Host $override,
+Write-Host $publishWorkflows,
+Write-Host $overwriteUnmanagedCustomizations,
+Write-Host $skipProductUpdateDependencies,
+Write-Host $convertToManaged,
+Write-Host $holdingSolution,
+Write-Host $AsyncWaitTimeout,
+Write-Host $logsDirectory,
+Write-Host $logFilename
