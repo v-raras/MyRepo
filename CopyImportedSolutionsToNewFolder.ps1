eb57775bc7d140d84b547c67488f4c745bf7a084
v-raras <50567545+v-raras@users.noreply.github.com>
2019-06-11 13:32:36 +0530

    Update CopyImportedSolutionsToNewFolder.ps1

--- CopyImportedSolutionsToNewFolder.ps1
+++ CopyImportedSolutionsToNewFolder.ps1
@@ -1,36 +1,10 @@
 param(
-$path,
-$solutionListFile,
-$solutionImportPath,
-$solutionFileName
+$solutionListFile
 )
-If (Test-Path -Path $path -PathType Container)
-
-    { 
-    Write-Host "$path already exists" -ForegroundColor Red
-    }
-
-    ELSE
-        { 
-        New-Item -Path $path  -ItemType directory 
-        }
 
         foreach($solution in [System.IO.File]::ReadLines($solutionListFile)){
-$solutionName = "$solution".Trim()
-Write-Host $solutionName
-        $solutionPath = "$solutionImportPath\$solutionName "
-
-        Copy-Item -Path $solutionPath  -Destination $path
-        Write-Host "solution file copied from " $solutionImportPath " to " $path " "
-        
-        $solutionFileName +=  "$solution" + ";"
+$solutionFileName +=  "$solution" + ";"
         }
-
-If (Test-Path -Path $path -PathType Container)
-
-    { 
-    Write-Host "$path already exists" -ForegroundColor Red
-    }
 
 $newlineDelimited = $solutionFileName -replace ';', "%0D%0A"
 
