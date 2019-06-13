01815878b5c32755b382743a79aae402fe0d2367
v-raras <50567545+v-raras@users.noreply.github.com>
2019-06-13 09:30:31 +0530

    Update CopyImportedSolutionsToNewFolder.ps1

--- CopyImportedSolutionsToNewFolder.ps1
+++ CopyImportedSolutionsToNewFolder.ps1
@@ -9,14 +9,3 @@ $solutionFileName +=  "$solution" + ";"
 $newlineDelimited = $solutionFileName -replace ';', "%0D%0A"
 
 Write-Host "##vso[task.setvariable variable=SolutionsFileName]$newlineDelimited"
-
-If (Test-Path -Path $path -PathType Container)
-
-    { 
-    Write-Host "$path already exists" -ForegroundColor Red
-    }
-
-    ELSE
-        { 
-        New-Item -Path $path  -ItemType directory 
-        }
