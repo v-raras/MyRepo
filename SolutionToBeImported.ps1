b4abc76b94b09575746568a063e8c49c28cfe4d1
IGD service account 36 <TestSolutionCommitterService@microsoft.com>
2019-06-28 19:31:09 +0530

    test 1

---
+++ SolutionToBeImported.ps1
@@ -0,0 +1,10 @@
+﻿param(
+$solutionListFile
+)
+        foreach($solution in [System.IO.File]::ReadLines($solutionListFile)){
+$solutionFileName +=  "$solution" + ";"
+        }
+
+$newlineDelimited = $solutionFileName -replace ';', "%0D%0A"
+
+Write-Host "##vso[task.setvariable variable=SolutionsFileName]$newlineDelimited"
---
+++ Test/Release/MasterSolution1/WebResources/syed_testWebResource.js
@@ -0,0 +1,4 @@
+function TestAlert()
+{
+alert("Test");
+}
